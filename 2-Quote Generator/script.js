6ba5db9e465fb118797c94e15710f1444d29a12f
Mustafa Lanewala <125785421+mustafalanewala@users.noreply.github.com>
2024-02-10 21:33:55 +0530

    Update script.js

--- 2-Quote Generator/script.js
+++ 2-Quote Generator/script.js
@@ -7,19 +7,39 @@ const newQuoteBtn = document.getElementById('new-quote-btn');
 
 newQuoteBtn.addEventListener('click', () => {
     getQuote();
-    changeBackgroundImage();
 });
 
-function getQuote() {
-    fetch('https://api.quotable.io/random')
-        .then(response => response.json())
-        .then(data => {
-            quoteText.textContent = `"${data.content}"`;
-            authorText.textContent = `- ${data.author}`;
-            const now = new Date();
-            dateTimeText.textContent = `${now.toLocaleDateString()} ${now.toLocaleTimeString()}`;
-        })
-        .catch(error => console.error('Error fetching quote:', error));
+async function getQuote() {
+    const apiUrl = 'https://api.api-ninjas.com/v1/quotes';
+    
+    try {
+        const response = await fetch(apiUrl, {
+            headers: {
+                'X-Api-Key': '3+044zhLHVVKEkgr9YdT9A==9yLgC9jTMMecbsjf'
+            }
+        });
+        
+        if (!response.ok) {
+            throw new Error('Failed to fetch quote');
+        }
+        
+        const data = await response.json();
+        const randomIndex = Math.floor(Math.random() * data.length);
+        const quoteData = data[randomIndex];
+        
+        const quote = quoteData.quote;
+        const author = quoteData.author;
+        
+        quoteText.textContent = `"${quote}"`;
+        authorText.textContent = `- ${author}`;
+        
+        const now = new Date();
+        dateTimeText.textContent = `${now.toLocaleDateString()} ${now.toLocaleTimeString()}`;
+        
+        changeBackgroundImage();
+    } catch (error) {
+        console.error('Error fetching quote:', error);
+    }
 }
 
 function changeBackgroundImage() {
